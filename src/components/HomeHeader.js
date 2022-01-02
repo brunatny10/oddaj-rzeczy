20ef8969a7e6409698e01f5b63437404cb4c2d93
brunatny10 <brunatny10@gmail.com>
2022-01-02 12:52:29 +0100

    homeHeader and create HomeThreeColumns

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -80,10 +80,10 @@ const HomeHeader = () => {
                     </div>
                     <div className="downMenuDown">
                         <div className="downMenuDown_Box">
-                            <Link2 className="Link2" to="/logowanie">Logowanie</Link2>
-                        </div>
-                        <div className="downMenuDown_Box">
                             <Link2 className="Link2" to="/logowanie">Zorganizuj zbiórke</Link2>
+                            </div>
+                        <div className="downMenuDown_Box">   
+                            <Link2 className="Link2" to="/logowanie">Zorganizuj<br/> zbiórke</Link2>
                         </div>
                     </div>
                 </div>
