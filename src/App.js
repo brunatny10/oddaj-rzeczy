d5f6597e9d0835b133657dd05591fbf5aa82a254
brunatny10 <brunatny10@gmail.com>
2022-01-14 13:58:38 +0100

    add changes app.js and home.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import { Routes, Route } from "react-router";
 import Home from "./components/Home";
 import SignIn from "./components/SignIn";
 import Register from "./components/Register";
+import SignOut from "./components/SignOut";
+import Form from "./components/Form/Form";
 
 function App() {
   return(
       <BrowserRouter>
         <Routes>
-          <Route exact path="/" element={<Home />}/>
-          <Route exact path="/logowanie" element={<SignIn />}/>
-          <Route exact path="/rejestracja" element={<Register />}/>
+            <Route exact path="/" element={<Home />}/>
+            <Route exact path="/logowanie" element={<SignIn />}/>
+            <Route exact path="/rejestracja" element={<Register />}/>
+            <Route exact path="/wylogowano" element={<SignOut />}/>
+            <Route exact path="/oddaj-rzeczy" element={<Form />}/>
         </Routes>
       </BrowserRouter>
   );
